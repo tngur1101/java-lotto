9415670a6c29e1478f4efeb4b453241b625c4671
kimsuhyeok <tngur1101@naver.com>
2023-09-03 16:22:49 +0900

    feat: 사용자들이 보는 View 클래스를 구현하였다.
    사용자에게 보여지는 부분인 View 클래스와 print할 함수들을 구현하였다.

---
+++ src/main/java/view/ViewLotto.java
@@ -0,0 +1,36 @@
+package view;
+
+public class ViewLotto {
+
+    static final String GET_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    static final String ALERT_PURCHASED_LOTTO = "%d개를 구매했습니다.";
+    static final String GET_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    static final String GET_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    static final String WIN_DATA = "당첨 통계";
+    static final String LINE = "---";
+    static final String MATCH_NUMBER_COUNT = "%d개 일치";
+    static final String MATCH_LOTTO_COUNT = "%d개";
+    static final String TOTAL_EARNING_RATE = "총 수익률은 %d입니다.";
+
+    public static void askPurchasePrice(){
+        System.out.println(GET_PURCHASE_PRICE);
+    }
+
+    public static void askLottoNumber(){
+        System.out.println(GET_LOTTO_NUMBER);
+    }
+
+    public static void askBonusNumber(){
+        System.out.println(GET_BONUS_NUMBER);
+    }
+
+    public static void printData(){
+        System.out.println(WIN_DATA);
+        System.out.println(LINE);
+    }
+
+    public static void printEarningRate(){
+        System.out.println(TOTAL_EARNING_RATE);
+    }
+
+}
