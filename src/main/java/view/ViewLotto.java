2b1edc29fe9c8876bbce7b954cc6cce0a68d4fcb
kimsuhyeok <tngur1101@naver.com>
2023-09-03 17:40:44 +0900

    Fix: ViewLotto와 ValidateView 분리
    ViewLotto안에 들어있던 유효 판단 출력을 ValidateView와 분리하였다.

--- src/main/java/view/ViewLotto.java
+++ src/main/java/view/ViewLotto.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.List;
+
 public class ViewLotto {
 
     static final String GET_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
@@ -11,6 +13,9 @@ public class ViewLotto {
     static final String MATCH_NUMBER_COUNT = "%d개 일치";
     static final String MATCH_LOTTO_COUNT = "%d개";
     static final String TOTAL_EARNING_RATE = "총 수익률은 %d입니다.";
+    static final String PURCHASE_PRICE_IS_NOT_NUMBER = "구매 금액이 숫자가 아닙니다.";
+    static final String PURCHASE_PRICE_CANNOT_DIVDE_WITH_THOUSAND = "구매 금액이 1000원단위가 아닙니다.";
+
 
     public static void askPurchasePrice(){
         System.out.println(GET_PURCHASE_PRICE);
@@ -18,6 +23,10 @@ public class ViewLotto {
 
     public static void askLottoNumber(){
         System.out.println(GET_LOTTO_NUMBER);
+    }
+
+    public static void printLottoNumber(List<Integer> numbers){
+        System.out.println(numbers);
     }
 
     public static void askBonusNumber(){
