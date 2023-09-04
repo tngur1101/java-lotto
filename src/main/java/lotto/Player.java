af328257f91af174d26b4d6bbbaca8de475dbf20
kimsuhyeok <tngur1101@naver.com>
2023-09-04 13:33:49 +0900

    feat : Player 당첨 번호 및 보너스 번호 입력
    당첨 번호와 보너스 번호를 입력받는다.

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -1,26 +1,71 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import view.ValidateView;
 import view.ViewLotto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
 
     int playerPurchasePrice;
     int lottoCount;
     static final int LOTTO_PRICE = 1000;
-
-
+    List<Lotto> lottoList = new ArrayList<>();
+    int[] playerLottoNumbers;
+    int bonus;
 
     public Player() {
-        ViewLotto.askPurchasePrice();
-        String playerInput = Console.readLine();
-        validatePurchasePriceInput(playerInput);
-        playerPurchasePrice = Integer.parseInt(playerInput);
-
+        purchaseLotto();
+        makeLotto();
+        inputLotto();
+        inputBonus();
     }
 
-    public void validatePurchasePriceInput(String playerInput){
+    public void purchaseLotto(){
+        ViewLotto.askPurchasePrice();
+        String playerInput = Console.readLine();
+        validateInputIsNumber(playerInput);
+        playerPurchasePrice = Integer.parseInt(playerInput);
+        validateCanDivideWithThousand();
+        lottoCount = playerPurchasePrice/LOTTO_PRICE;
+        ViewLotto.alertPurchasedLottoCount(lottoCount);
+    }
+
+    public void inputLotto(){
+        ViewLotto.askLottoNumber();
+        String playerLottoNumber = Console.readLine();
+        String[] playerLotto = playerLottoNumber.split(",");
+        playerLottoNumbers = new int[playerLotto.length];
+        for(int i=0;i<playerLotto.length;i++){
+            validateInputIsNumber(playerLotto[i]);
+            playerLottoNumbers[i] = Integer.parseInt(playerLotto[i]);
+        }
+    }
+
+    public void inputBonus(){
+        ViewLotto.askBonusNumber();
+        String playerBonusNumber = Console.readLine();
+        validateInputIsNumber(playerBonusNumber);
+        bonus = Integer.parseInt(playerBonusNumber);
+    }
+
+    public void makeLotto(){
+        for(int i=0;i<lottoCount;i++){
+            lottoList.add(new Lotto(Lotto.createLottoNumber()));
+        }
+    }
+
+    public void sortAndPrintLotto(){
+        for(int i=0;i<lottoCount;i++){
+             Lotto.sortLottoNumber(lottoList.get(i).getLotto());
+             ViewLotto.printLottoNumber(lottoList.get(i).getLotto());
+        }
+    }
+
+    public void validateInputIsNumber(String playerInput){
         try{
             Integer.parseInt(playerInput);
         }catch (NumberFormatException e){
@@ -30,11 +75,11 @@ public class Player {
     }
 
     public void validateCanDivideWithThousand(){
-        if(playerPurchasePrice % 1000 != 0){
+        if(playerPurchasePrice % LOTTO_PRICE != 0){
             ValidateView.purchasePriceCannotDivdeWithThousand();
             throw new IllegalArgumentException();
         }
-        if(playerPurchasePrice < 1000){
+        if(playerPurchasePrice < LOTTO_PRICE){
             ValidateView.purchasePriceUnderThousand();
             throw new IllegalArgumentException();
         }
